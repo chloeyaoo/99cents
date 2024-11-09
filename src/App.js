515e02985df61e32d4b8f0a338f6e7a6b5ac18b7
chloeyaoo <chloeyieee@gmail.com>
2024-11-09 16:35:22 +0800

    Update App.js to use components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import SendAppreciation from './components/SendAppreciation';
+import ActivityHistory from './components/ActivityHistory';
+import Leaderboard from './components/Leaderboard';
+import Analytics from './components/Analytics';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/send" element={<SendAppreciation />} />
+        <Route path="/history" element={<ActivityHistory />} />
+        <Route path="/leaderboard" element={<Leaderboard />} />
+        <Route path="/analytics" element={<Analytics />} />
+      </Routes>
+    </Router>
   );
 }
 
