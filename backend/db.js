f3e1be86e13b0392f1516442d20cb6495e8f828c
chloeyaoo <chloeyieee@gmail.com>
2024-11-14 03:42:49 +0800

    Troubleshoot

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,21 @@
+// const { Pool } = require('pg');
+// const pool = new Pool({
+//   user: 'postgres',
+//   host: 'localhost',
+//   database: 'appreciation',
+//   // password: 'your_password',
+//   port: 5432,
+// });
+
+// module.exports = pool;
+
 const { Pool } = require('pg');
+
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'appreciation',
-  // password: 'your_password',
-  port: 5432,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,  // Use this to avoid SSL issues on Render
+  },
 });
 
 module.exports = pool;
