4f0431e37af92433699a2a443e7f9bf836f10506
chloeyaoo <chloeyieee@gmail.com>
2024-11-14 02:02:20 +0800

    Misc

--- frontend/src/apiConfig.js
+++ frontend/src/apiConfig.js
@@ -1,5 +1,5 @@
 // src/apiConfig.js
 
-const API_BASE_URL = "https://nine9cents-backend.onrender.com/api";
+const API_BASE_URL = "https://nine9cents-backend.onrender.com";
 
 export default API_BASE_URL;
